bd3726f12a93975e83834f6be2de417e30c3af53
fatmashaban0@gmail.com <fs5865@gadi-login-08.gadi.nci.org.au>
2026-05-01 11:57:34 +1000

    The include AGN flag file modifications

--- src/core/read_params.c
+++ src/core/read_params.c
@@ -402,6 +402,11 @@ void read_parameter_file(char* fname, int mode)
       params_addr[n_param] = &(run_params->FlagInteractive);
       required_tag[n_param] = 1;
       params_type[n_param++] = PARAM_TYPE_INT;
+     
+      strncpy(params_tag[n_param], "Flag_includeAGN", tag_length);
+      params_addr[n_param] = &(run_params->physics.Flag_includeAGN);
+      required_tag[n_param] = 1;
+      params_type[n_param++] = PARAM_TYPE_INT;
 
       strncpy(params_tag[n_param], "FlagMCMC", tag_length);
       params_addr[n_param] = &(run_params->FlagMCMC);
@@ -1117,6 +1122,11 @@ void read_parameter_file(char* fname, int mode)
 
       strcpy(params_tag[n_param], "SpecIndexXrayGal");
       params_addr[n_param] = &(run_params->physics).SpecIndexXrayGal;
+      required_tag[n_param] = 1;
+      params_type[n_param++] = PARAM_TYPE_DOUBLE;
+
+      strcpy(params_tag[n_param], "LXrayAGN");
+      params_addr[n_param] = &(run_params->physics).LXrayAGN;
       required_tag[n_param] = 1;
       params_type[n_param++] = PARAM_TYPE_DOUBLE;
 
